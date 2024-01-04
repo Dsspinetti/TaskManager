68d0dfab899bfa5c6578420be96773856a0f0d04
Dominick Spinetti <spinettidominick@gmail.com>
2024-01-04 13:21:04 -0700

    More clean up of unused methods

--- src/main/java/com/dominick/taskmanager/Task.java
+++ src/main/java/com/dominick/taskmanager/Task.java
@@ -19,26 +19,11 @@ public class Task implements Serializable {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Date getDueDate() {
-        return dueDate;
-
-    }
-
-    public Date setDueDate(Date dueDate) {
-        this.dueDate = dueDate;
         return dueDate;
     }
 
     public String getStatus() {
-        return status;
-    }
-
-    public String setStatus(String status) {
-        this.status = status;
         return status;
     }
 
@@ -50,17 +35,5 @@ public class Task implements Serializable {
         this.status = "In Progress";
     }
 
-    public void displayTaskDetails() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println("Task Details:");
-        System.out.println("Name: " + name);
-        System.out.println("Due Date: " + dateFormat.format(dueDate));
-        System.out.println("Status: " + status);
-    }
-
-    public String getFormattedDueDate() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        return sdf.format(dueDate);
-    }
 }
 
